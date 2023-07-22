f9157c0df53b26360a37a5ad43e8c5be4865d0da
kushagra vatsa <kushagravats911@gmail.com>
2023-07-22 17:29:17 +0530

    For_Loops.cpp

---
+++ for_loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //size_t : a representation of some unsigned int for some positive number
+    int count=10;
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+        cout<<"I like C++ "<<endl;
+    }
+    cout<<"Loop Done!"<<endl;
+    return 0;
+}
